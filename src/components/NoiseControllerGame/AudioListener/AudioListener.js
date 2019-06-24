83e4c416295f15e41a4a185d105f55a0b36e31ee
maxdavid <me@maxdavid.com>
2019-06-24 15:25:41 -0700

    Add basic Game and AudioListener components
    Game has a tick function that triggers every half-second keeping track
    of time since microphone was activated.
    Game keeps track of audio threshold as a number and aboveThreshold
    boolean in state. A function is passed to the AudioListener that
    triggers when audio is above threshold.

---
+++ src/components/NoiseControllerGame/AudioListener/AudioListener.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class AudioListener extends React.Component {
+  componentDidMount() {
+    this.audioContext = new (window.AudioContext ||
+      window.webkitAudioContext)();
+    this.analyser = this.audioContext.createAnalyser();
+    this.audioData = new Uint8Array(this.analyser.frequencyBinCount);
+    this.source = this.audioContext.createMediaStreamSource(this.props.audio);
+    this.source.connect(this.analyser);
+  }
+
+  componentDidUpdate() {
+    this.analyser.getByteTimeDomainData(this.audioData);
+    if (Math.max.apply(Math, this.audioData) > this.props.threshold) {
+      this.props.handleAboveThreshold();
+    }
+  }
+
+  componentWillUnmount() {
+    cancelAnimationFrame(this.rafId);
+    this.analyser.disconnect();
+    this.source.disconnect();
+  }
+
+  render() {
+    return <div>🎤</div>;
+  }
+}
+
+export default AudioListener;
--- src/components/NoiseControllerGame/Game.js
+++ src/components/NoiseControllerGame/Game.js
@@ -1,8 +1,91 @@
 import React from 'react';
+import AudioListener from './AudioListener/AudioListener';
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      audio: null,
+      threshold: 130,
+      ticks: 0,
+      aboveThreshold: false
+    };
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  startTimer = () => {
+    clearInterval(this.timer);
+    this.timer = setInterval(this.tick, 500);
+  };
+
+  stopTimer = () => {
+    clearInterval(this.timer);
+  };
+
+  getMicrophone = async () => {
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false
+    });
+    this.setState({ audio });
+  };
+
+  stopMicrophone = () => {
+    this.state.audio.getTracks().forEach(track => track.stop());
+    this.setState({ audio: null });
+  };
+
+  handleAboveThreshold = () => {
+    function multipleViolations(state) {
+      if (state.aboveThreshold) return null;
+      return {
+        aboveThreshold: true
+      };
+    }
+    this.setState(multipleViolations);
+  };
+
+  toggleMicrophone = text => {
+    console.log(text);
+    if (this.state.audio) {
+      this.stopMicrophone();
+      this.stopTimer();
+    } else {
+      this.getMicrophone();
+      this.startTimer();
+    }
+  };
+
+  tick = () => {
+    this.setState(prevState => ({
+      ticks: prevState.ticks + 1,
+      aboveThreshold: false
+    }));
+  };
+
   render() {
-    return <h1>Game</h1>;
+    return (
+      <div className='Game'>
+        <button
+          className='controls'
+          onClick={() => this.toggleMicrophone('wat')}
+        >
+          {this.state.audio ? 'Stop Mic' : 'Start Mic'}
+        </button>
+        {this.state.audio && (
+          <div>
+            <AudioListener
+              {...this.state}
+              handleAboveThreshold={this.handleAboveThreshold}
+            />
+            {this.state.aboveThreshold ? <h1>loud</h1> : <h1>quiet</h1>}
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
